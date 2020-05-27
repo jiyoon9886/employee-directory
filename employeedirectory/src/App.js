7789c69d9452d688c389a87ee1687d2494e4900b
Jiyoon Kim <jiyoon9886@gmail.com>
2020-05-27 15:34:16 -0400

    set up git repo and created Header.js

---
+++ employeedirectory/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import './App.css';
+import Header from "./components/Header";
+
+//main container (flex-direction: column)
+//header
+
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Header />
+			</header>
+		</div>
+	);
+}
+
+//search
+//table
+//columns titles
+
+export default App;
---
+++ employeedirectory/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+	return (
+		<div className="jumbotron jumbotron-fluid">
+			<div className="container">
+				<h1 className="display-4">Employee Directory</h1>
+				<p className="lead">Search for your teammates by name.</p>
+			</div>
+		</div>
+	)
+}
+
+export default Header;
